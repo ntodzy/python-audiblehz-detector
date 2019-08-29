6e7ecb1e2e49b2f2f7cc8e7d0ebe2aeed4045185
Skyguy92 <Skyguy92@users.noreply.github.com>
2019-08-28 21:48:21 -0500

    unspaghettied code & added interaction support!

--- frequency_detector.py
+++ frequency_detector.py
@@ -1,10 +1,12 @@
 import numpy as np
 import pyaudio
-from scipy import fftpack
+# import matplotlib for other stuff later <3
+# import pyautogui 
 
+#dont define filesystem as fs thx
 FREQ_ARRAY = []
-NOTE_MIN = 60 #C4
-NOTE_MAX = 80 #A4
+NOTE_MIN = 20 #C4 = 60
+NOTE_MAX = 80 #A4 = 69
 FSAMP = 22050 #idk should be 1024 but ok?
 FRAME_SIZE = 2048
 FRAMES_PER_FFT = 32 # Average Across FFT (AKA REMOVE THE OVERTONES)
@@ -16,7 +18,7 @@ NOTE_NAMES = 'C C# D D# E F F# G G# A A# B'.split()
 
 def freq_to_number(f): return 69 + 12*np.log2(f/440.0)
 def number_to_freq(n): return 440 * 2.0**((n-69)/12.0)
-def note_name(n): return NOTE_NAMES[n % 12] + str(n/12 - 1)
+def note_name(n): return NOTE_NAMES[n % 12] #+ str(n/12 - 1)
 
 #MINMAX INDEX
 
@@ -28,6 +30,7 @@ imax = min(SAMPLES_PER_FFT, int(np.ceil(note_to_fftbin(NOTE_MAX+1))))
 buf = np.zeros(SAMPLES_PER_FFT, dtype=np.float32)
 num_frames = 0
 
+#Create a live audio data channel. Pyaudio will create a numpy array for the fft
 stream = pyaudio.PyAudio().open(format=pyaudio.paInt16,
                                 channels=1,
                                 rate=FSAMP,
@@ -40,6 +43,22 @@ window = .5 * (1 - np.cos(np.linspace(0, 2*np.pi, SAMPLES_PER_FFT, False)))
 
 print ('sampling at', FSAMP, 'Hz with Max Resolutoin of', FREQ_STEP, 'Hz')
 print
+
+#Live User Interaction
+#interactions = dict({'B': b, 'C':c,'C#':cs,'D':d,'D#':ds,'E':e,'F':f, 'F#':fs, 'G':g, 'G#':gs, 'A': a , 'A#': ash}) # This next section is for post-interactions, aka playing beeps writing more code, exporting certain things when certain notes happen! Please leave it all commented if you dont want that to happen.
+
+# def b() :
+# def c() :
+# def cs() :
+# def d() :
+# def ds() :
+# def e() :
+# def f() :
+# def fs() :
+# def g() :
+# def gs() :
+# def a() :
+# def ash() :
 
 while stream.is_active():
         
@@ -57,8 +76,8 @@ while stream.is_active():
     num_frames += 1
 
     if num_frames >= FRAMES_PER_FFT:
-        print (freq) #Frequency (hz)
         FREQ_ARRAY.append(freq) # Add to Freq Array
-        print (len(FREQ_ARRAY))
-        #print(n0) # Number in the equal temperment thing
-       # print(num_frames)
+        print(note_name(n0), freq, len(FREQ_ARRAY)) #Run N0 through note name function to retreive note
+
+        # interactionToCall = interactions[note_name(n0).toString()] # These two lines are for serving out the interactions. If you dont want any interactions please leave commented! 
+        # interactionToCall() 
